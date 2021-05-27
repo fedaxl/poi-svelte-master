09a8be201f22f9e6ab1fdd8343fc989dd2ae25c8
Federica Fiorenza <20091413@mail.wit.ie>
2021-05-27 18:12:06 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -23,7 +23,7 @@
     "/login": Login,
     "/categories": Categories,
     "/home": Pois,
-    "/pois/{id}": ViewPoi,
+    "/view-poi/{id}": ViewPoi,
     "/categories/{id}/pois": AddPoi,
     "/add-category": AddCategory,
     "/categories/{id}": ViewCategory,
